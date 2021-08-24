744e2000b562cc3c49fd9dd21157aed1d71b370f
Lorenzo <pirolorenzo@live.it>
2021-08-24 20:20:19 +0200

    tttttt

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -7,21 +7,21 @@ let nextImageNumber = 0;
 let numOfImages = 7;
 
 nextButton.addEventListener("click", function(){
-    nextImageNumber = (nextImageNumber + 1) % numOfImages;
-    trailerImage.setAttribute("src", "images/trailerImages/" + nextImageNumber + ".jpg");
-    nextButton.setAttribute("src", "images/nextPressed.png")
     setTimeout(function(){
         nextButton.setAttribute("src", "images/next.png")
     }, 100);
+    nextImageNumber = (nextImageNumber + 1) % numOfImages;
+    trailerImage.setAttribute("src", "images/trailerImages/" + nextImageNumber + ".jpg");
+    nextButton.setAttribute("src", "images/nextPressed.png")
 });
 
 prevButton.addEventListener("click", function(){
+    setTimeout(function(){
+        prevButton.setAttribute("src", "images/prev.png")
+    }, 100);
     nextImageNumber = ((nextImageNumber - 1) % numOfImages + numOfImages) % numOfImages;
     trailerImage.setAttribute("src", "images/trailerImages/" + nextImageNumber + ".jpg");
     prevButton.classList.add(".pressed");
     prevButton.setAttribute("src", "images/prevPressed.png")
-    setTimeout(function(){
-        prevButton.setAttribute("src", "images/prev.png")
-    }, 100);
 });
 
