44e8c7307b8e5233d5e474469cb5fb2a30a3d11c
Lorenzo <pirolorenzo@live.it>
2021-08-24 17:55:26 +0200

    Primo commit

---
+++ src/main/java/com/example/progettoweb/ProgettoWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.progettoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoWebApplication.class, args);
+    }
+
+}
