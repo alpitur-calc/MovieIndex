8dd7cbf9e545de017c3b94df02aa7a2aa94ab5b4
manu <calabrettaemanuele@gmail.com>
2021-09-08 18:21:22 +0200

    fix watchList in userpage

--- src/main/java/com/example/progettoweb/controller/userProfileController.java
+++ src/main/java/com/example/progettoweb/controller/userProfileController.java
@@ -1,30 +1,13 @@
 package com.example.progettoweb.controller;
 
 import model.User;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import persistence.DBManager;
-import persistence.dao.jdbc.UserDaoJDBC;
 
-import javax.imageio.ImageIO;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.awt.*;
-import java.awt.image.RenderedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
 
 @Controller
 public class userProfileController {
@@ -38,6 +21,18 @@ public class userProfileController {
             model.addAttribute("biography", user.getBiography());
             if(user.getProfileImage() != null){ model.addAttribute("profileimage", user.getProfileImage()); }
             else{ model.addAttribute("profileimage", "default"); }
+
+            if(user.getWatchList() != null) {
+                if(user.getWatchList().size()>0)
+                    model.addAttribute("watchList", user.getWatchList());
+            }
+
+            //In moviePage :
+            // <c:ForEach var="movie" items=${watchlisti}> SCORRE TUTTI GLI ELEM DELLA LISTA CHE TROVA NEL MODEL
+            //      QUI METTI QUELLO CHE VUOI IN BASE A COME VISUALIZZI LA REVIEW
+            //      per accedere ai dati di una review basta fare ${movie.<attributo>}
+            //      ES: ${movie.content}
+            // </c:ForEach>
 
             return "userProfile";
         }
