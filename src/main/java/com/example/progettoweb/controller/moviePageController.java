62195053a3cdc6392293abc0f5d0cedbab06add6
manu <calabrettaemanuele@gmail.com>
2021-09-10 17:12:45 +0200

    aggiunto removefromwatchlist

--- src/main/java/com/example/progettoweb/controller/moviePageController.java
+++ src/main/java/com/example/progettoweb/controller/moviePageController.java
@@ -19,30 +19,28 @@ import java.util.List;
 public class moviePageController {
 
     @GetMapping("/movie")
-    public String moviePage(Model model, @RequestParam int movieId){
-
+    public String moviePage(HttpSession session, Model model, @RequestParam int movieId){
         Movie movie = DBManager.getInstance().movieDao().findByPrimaryKey(movieId);
         if(movie.getId() != 0){
-            List<Review> reviews = DBManager.getInstance().reviewDao().findAllReviewOfAFilm(movie);
+            /*List<Review> reviews = DBManager.getInstance().reviewDao().findAllReviewOfAFilm(movie);
             if(reviews != null){
                  model.addAttribute("reviewsList", reviews);
-            }
+            }*/
         }
-
         else{
             movie = new Movie();
             movie.setId(movieId);
         }
+        String added = "false";
+        try{
+            User user = DBManager.getInstance().userDao().findByPrimaryKey((String) session.getAttribute("userlogged"));
+            List<Integer> watchlist = user.getWatchList();
+            if(watchlist.contains(movieId)){ added = "true";}
+        }catch(Exception e){}
 
+        model.addAttribute("added", added);
         model.addAttribute("movieId", movie.getId());
-        System.out.println(movie.getId());
 
-        //In moviePage :
-        // <c:ForEach var="review" items=${reviews}> SCORRE TUTTI GLI ELEM DELLA LISTA CHE TROVA NEL MODEL
-        //      QUI METTI QUELLO CHE VUOI IN BASE A COME VISUALIZZI LA REVIEW
-        //      per accedere ai dati di una review basta fare ${review.<attributo>}
-        //      ES: ${review.content}
-        // </c:ForEach>
         return "moviePage";
     }
 
@@ -75,6 +73,34 @@ public class moviePageController {
         return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
     }
 
+    @PostMapping("/removeMovieFromWatchlist")
+    public ResponseEntity<String> removeMovieFromWatchlist(HttpSession session, @RequestParam Integer movieId){
+        User user = DBManager.getInstance().userDao().findByPrimaryKey((String) session.getAttribute("userlogged"));
+
+        Movie movie = DBManager.getInstance().movieDao().findByPrimaryKey(movieId);
+        if(movie.getId() == 0) {
+            movie = new Movie();
+            movie.setId(movieId);
+            DBManager.getInstance().movieDao().save(movie);
+        }
+
+        if(user != null && movie.getId() != 0) {
+            List<Integer> list = user.getWatchList();
+            list.remove(movie.getId());
+
+            User newUser = new User();
+            newUser.setUsername(user.getUsername());
+            newUser.setPassword("");
+            newUser.setEmail(user.getEmail());
+            newUser.setBiography(user.getBiography());
+            newUser.setProfileImage(user.getProfileImage());
+            newUser.setWatchList(list);
+
+            DBManager.getInstance().userDao().update(newUser, user);
+            return new ResponseEntity<String>(HttpStatus.OK);
+        }
+        return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+    }
     @PostMapping("/addReviewToMovie")
     public boolean addReviewToMovie(){
         return false;
