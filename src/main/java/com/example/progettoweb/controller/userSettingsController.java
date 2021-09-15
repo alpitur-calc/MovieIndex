d7753adf58591e37634299760128e503cb5e830e
manu <calabrettaemanuele@gmail.com>
2021-09-15 17:36:58 +0200

    fix a userSettingController

--- src/main/java/com/example/progettoweb/controller/userSettingsController.java
+++ src/main/java/com/example/progettoweb/controller/userSettingsController.java
@@ -80,7 +80,7 @@ public class userSettingsController {
         if(newUser.getProfileImage() != null){ model.addAttribute("profileimage", newUser.getProfileImage()); }
         else{ model.addAttribute("profileimage", "default"); }
 
-        return "userProfile";
+        return "/userProfile";
     }
 
     @PostMapping("/savePassword")
@@ -104,7 +104,7 @@ public class userSettingsController {
             if(newUser.getProfileImage() != null){ model.addAttribute("profileimage", newUser.getProfileImage()); }
             else{ model.addAttribute("profileimage", "default"); }
 
-            return "userProfile";
+            return "/userProfile";
         }
 
         model.addAttribute("setting", "password");
@@ -131,11 +131,11 @@ public class userSettingsController {
             model.addAttribute("username", newUser.getUsername());
             model.addAttribute("biography", newUser.getBiography());
             model.addAttribute("profileimage", newUser.getProfileImage());
-            return "userProfile";
+            return "/userProfile";
         }catch (Exception e){}
 
         model.addAttribute("failedToSave", "true");
-        return "userSetting";
+        return "/userSetting";
     }
 
 }
