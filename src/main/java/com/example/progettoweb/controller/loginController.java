ae196c56b1340468f2555e39983a2a0702f4e902
manu <calabrettaemanuele@gmail.com>
2021-09-16 11:12:24 +0200

    aggiunto rememberMe

--- src/main/java/com/example/progettoweb/controller/loginController.java
+++ src/main/java/com/example/progettoweb/controller/loginController.java
@@ -4,26 +4,42 @@ import model.Encrypter;
 import model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import persistence.DBManager;
 
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 
 @Controller
 public class loginController {
 
     @GetMapping("/logIn")
-    public String logIn(){
+    public String logIn(@CookieValue(value = "userlogged") String username,
+                        @CookieValue(value = "password") String password, Model model){
+
+        if(username != null && password != null){
+            model.addAttribute("username", username);
+            model.addAttribute("password", password);
+        }
         return "logIn";
     }
 
     @PostMapping("/doLogIn")
-    public String doLogIn(HttpSession session, Model model, @RequestParam String username, @RequestParam String password){
+    public String doLogIn(HttpSession session, HttpServletResponse response, Model model, @RequestParam String username,
+                          @RequestParam String password, @RequestParam String rememberMe){
         User user = DBManager.getInstance().userDao().findByPrimaryKey(username);
         if(user != null && Encrypter.check(password, user.getPassword())){
             session.setAttribute("userlogged", user.getUsername());
+            if(rememberMe.equals("yes")){
+                Cookie cookieUsr = new Cookie("userlogged",username);
+                Cookie cookiePsw = new Cookie( "password", password);
+                cookieUsr.setMaxAge(7 * 24 * 60 * 60); // Scade in 7 giorni
+                cookiePsw.setMaxAge(7 * 24 * 60 * 60); // Scade in 7 giorni
+                response.addCookie(cookieUsr);
+                response.addCookie(cookiePsw);
+            }
         }
         else{
             model.addAttribute("failedtologin", "true");
